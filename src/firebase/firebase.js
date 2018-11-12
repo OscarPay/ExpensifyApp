6843959aa01990b83e1858624265fdae5ae14afd
Oscar <oscar.pay4@gmail.com>
2018-11-12 12:24:39 -0600

    Setup test database env

---
+++ src/firebase/firebase.js
@@ -0,0 +1,60 @@
+import * as firebase from "firebase";
+
+const config = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
+};
+
+firebase.initializeApp(config);
+
+const database = firebase.database();
+
+export { firebase, database as default };
+
+// database.ref('expenses').push({
+//   description: 'Rent',
+//   note: '',
+//   amount: 109500,
+//   createdAt: 976123498763
+// });
+
+// database.ref('expenses').push({
+//   description: 'Phone Bill',
+//   note: '',
+//   amount: 5900,
+//   createdAt: 976123498763
+// });
+
+// database.ref('expenses').push({
+//   description: 'Food',
+//   note: '',
+//   amount: 1200,
+//   createdAt: 976123498763
+// });
+
+// database.ref().set({
+//   name: 'Oscar Pérez',
+//   age: 23,
+//   isSingle: true,
+//   location: {
+//     city: 'Mérida',
+//     country: 'México'
+//   }
+// }).then(() => {
+//   console.log('Data is saved');
+// }).catch((e) => {
+//   console.log('This failed', e);
+// })
+
+// database.ref()
+//   .remove()
+//   .then(() => {
+//     console.log('Data was removed');
+//   })
+//   .catch((e) => {
+//     console.log('Did not remove data ' + e);
+//   });
